e7493374e3546c213cd517c9cfbffc1991096d42
willeyzhou1 <willzhou000@gmail.com>
2025-02-21 17:56:01 -0800

    lil html website made :D

---
+++ script.js
@@ -0,0 +1,37 @@
+const image = document.querySelector(".image");
+const description = document.querySelector(".description");
+const yesButton = document.querySelector(".button.yes");
+const noButton = document.querySelector(".button.no");
+let buttonTextSize = 20;
+
+async function handleButton(button) {
+    if(button == yesButton) {
+        image.src = "./assets/smiley.jpeg";
+        description.innerHTML = "YIPPEEEEEEEE :DDDD say leth bbygurl";
+    }
+    else {
+        image.src = "./assets/dont.jpg";
+        description.innerHTML = "Don't even think about it."
+    }
+}
+
+function handleMouseOverOnce() {
+    handleButton(noButton);
+    noButton.removeEventListener("mouseover", handleMouseOverOnce);
+}
+
+yesButton.addEventListener("click", () => {
+    handleButton(yesButton);
+    noButton.style.visibility = "hidden";
+    yesButton.style.visibility = "hidden";
+});
+
+noButton.addEventListener("mouseover", handleMouseOverOnce);
+
+noButton.addEventListener("click", () => {
+    description.innerHTML = "sory this button doesnt work ;(( press the other one!!!!!!"
+    yesButton.style.width = `${yesButton.offsetWidth + 25}px`;
+    yesButton.style.height = `${yesButton.offsetHeight + 25}px`;
+    buttonTextSize += 5;
+    yesButton.style.fontSize = buttonTextSize + "px";
+})
